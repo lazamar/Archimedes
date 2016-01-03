bd36b47919b81b0dfd092c9bfab5dae3f95ca168
Marcelo Lazaroni <lazaronijunior@gmail.com>
2016-01-03 12:49:02 +0000

    Remove ignore

---
+++ js/AppKeys.js
@@ -0,0 +1,17 @@
+// This file holds passwords and API keys.
+
+var AppKeys = {
+    'weatherAPI': '5e68626ba3b7547ef8a34c0500c826cf',
+    'googleAPI': 'AIzaSyDm_1kT_cXkumBiPiMuMAGKJ-iyTsOMGEw',
+    'tflAPI': {
+        'appId': '1067605c',
+        'appKey': 'c4b254968398dca67e7082dfef435c7d'
+    },
+    'calendarAPI': {
+        //The access token is reset whenever it expires
+        'accessToken': 'ya29.MwI6CvP5iAN8FvsXKZgXkwCUWJJSWgGt5_Ul_Fe9shRH1bilq5OQEOYswSwcD8pt8Ixx',
+        'refreshToken': '1/7db8jLv1LgMy9-pgrJ9LisyvN3hqH7xzFofKABKFcLpIgOrJDtdun6zK6XiATCKT',
+        'clientId': '119974853322-gbvi7p9nbsbre8trghacs451jn5571s5.apps.googleusercontent.com',
+        'clientSecret': 'Xo2ZvAf4w9aODypptHV1aL5K'
+    }
+};
